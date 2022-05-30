d8ef08387b49d9b7fb6473026745729fa0b555fe
Long <you@example.com>
2022-05-30 22:51:20 +0700

    abc

--- main.js
+++ main.js
@@ -32,3 +32,53 @@ Nextimg = function next_img()
     img__bgreview.src = arr_img[index];
 }
 setInterval(Nextimg,3000);
+// Hiệu ứng chọn ảnh nhỏ mở ra ảnh lớn trong phần sản phẩm-products
+var img_products=[
+    "../assets/img/Uchiha Madara (2).jpg",
+    "../assets/img/Uchiha Madara (1).jpg",
+    "../assets/img/Uchiha Madara (3).jpg",
+    "../assets/img/Uchiha Madara (4).jpg",
+    "../assets/img/Uchiha Madara (5).jpg",
+]
+
+var dem = 0;
+
+function img_product1(){
+    dem = 0;
+    var img_products_big = document.getElementById("img_products_big");
+    img_products_big.src = img_products[dem];
+}
+
+function img_product2(){
+    dem = 1;
+    var img_products_big = document.getElementById("img_products_big");
+    img_products_big.src = img_products[dem];
+}
+
+function img_product3(){
+    dem = 2;
+    var img_products_big = document.getElementById("img_products_big");
+    img_products_big.src = img_products[dem];
+}
+
+function img_product4(){
+    dem = 3;
+    var img_products_big = document.getElementById("img_products_big");
+    img_products_big.src = img_products[dem];
+}
+
+function img_product5(){
+    dem = 4;
+    var img_products_big = document.getElementById("img_products_big");
+    img_products_big.src = img_products[dem];
+}
+
+// Nextimg_products = function next_img_products()
+// {
+//     dem++;
+//     if(dem >= img_products.length)
+//         dem = 0;
+//     var img__bgreview = document.getElementById("img_products_big");
+//     img__bgreview.src = img_products[dem];
+// }
+// setInterval(Nextimg_products,3000);
